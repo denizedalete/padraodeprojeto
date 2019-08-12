fb0c914af19b95860fe11bb9966e68bb52b720b1
denizedalete <54041159+denizedalete@users.noreply.github.com>
2019-08-12 16:39:49 -0300

    Update Singleton.py

--- Singleton.py
+++ Singleton.py
@@ -1,9 +1,33 @@
-class Singleton(object):
 
-    _instance = None
+# Singleton
 
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(
-                                      cls, *args, **kwargs)
-        return cls._instance
+# Primeiro Singleton por https://www.python.org/dev/peps/pep-0318/#examples
+def  singleton ( cls ):
+    instâncias = {}
+    def  getinstance ():
+        se  cls  não está  em instâncias:
+            instâncias [ cls ] =  cls ()
+        retornar instâncias [ cls ]
+    return getinstance
+
+@singleton
+classe MyClass
+
+# Segundo Singleton
+Classe  Singleton :
+    # Aqui será a instância armazenada.
+    __instance =  None
+
+    @ staticmethod
+    def  getInstance ():
+        "" " Método de acesso estático. " ""
+        if Singleton .__ instance ==  None :
+            Singleton ()
+        return singleton .__ instance
+
+    def  __init__ ( self ):
+        "" " Construtor virtualmente privado. " ""
+        if Singleton .__ instance ! =  None :
+            raise  Exception ( " Esta classe é um singleton! " )
+        else :
+            Singleton .__ instance =  self
